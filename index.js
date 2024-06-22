79d4443087b9fa2e4376c971758135ac222e69ae
unnnii <unnikhk@gmail.com>
2024-06-22 21:11:36 +0530

    trying to grasp basics
    learned how does the class "client" work and made it reply to a few messages (looking at discord.js documentation to learn)

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-
-
 const dotenv = require('dotenv');
-const {Client, ActivityType} = require("discord.js");
-const client = new Client({intents: []});
+const {Client, ActivityType, GatewayIntentBits} = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]});
 
 dotenv.config();
 
 
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`${client.user.tag} Online`);
 });
+
+
+
 
 client.login(process.env.TOKEN)
     .catch(error => {
         console.error('Failed to log in:', error);
     });
+
+
+    client.on("messageCreate", (message) => {
+        console.log(`${message.content}`)
+        console.log(`${message.author.tag}`)
+    } )
+
+client.on("messageCreate",(message) => {
+    if (message.content == "aide"||message.content ==  "aidee") {
+        message.reply("lorem impsum")
+    }
+})
+
+client.on("messageCreate",(message) => {
+    if (message.content == "shutdown") {
+        client.destroy()
+    }
+})
